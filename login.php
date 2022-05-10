39de9b34dd760d4f8902236cf672a43425e15e56
sistemas <sigpeauxsistemas@gmail.com>
2022-05-10 17:02:35 -0500

    first commit

---
+++ login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es-CO">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control Operacional</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="./css/responsive.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/2dd4f6d179.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="center">
+              
+            
+                <div class="card mt-responsive mx-auto div--center div--center-border" style="width: 18rem;">
+                    <div class="card-body">
+                 <img src="./assets/images/user.png" alt="User logo" class="img-fluid user--size user_icon--position-absolute">
+
+                        <div class="mb-3 mt-5 row">
+                            <div class="col-sm-12 mb-3">
+                                <input type="text" class="useFontAwesomeFamily form-control" id="staticEmail" placeholder="&#xf2bd; Usuario">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <div class="col-sm-12 mb-3">
+                                <input type="password" class="form-control" id="inputPassword" placeholder="Contraseña">
+                            </div>
+                        </div>
+
+                        <div class="mb-3 row">
+                            <div class="col-sm-12 mb-2">
+                                <div class="d-grid gap-2">
+                                    <button class="btn btn-danger rounded-pill" type="button">Ingresar</button>
+                               
+                                </div>
+                            </div>
+                        </div>
+
+                       
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
+</html>
